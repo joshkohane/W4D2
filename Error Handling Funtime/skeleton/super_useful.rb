d34b4419811c0344562eebc6f3af1260ccf516a4
joshkohane <joshkohane@gmail.com>
2020-10-13 22:15:08 -0400

    Edit super_useful.rb

--- Error Handling Funtime/skeleton/super_useful.rb
+++ Error Handling Funtime/skeleton/super_useful.rb
@@ -27,27 +27,29 @@ def feed_me_a_fruit
   puts "Feed me a fruit! (Enter the name of a fruit:)"
   maybe_fruit = gets.chomp
   reaction(maybe_fruit) 
+rescue
+  retry
 end  
 
-# # PHASE 4
-# class BestFriend
-#   def initialize(name, yrs_known, fav_pastime)
-#     @name = name
-#     @yrs_known = yrs_known
-#     @fav_pastime = fav_pastime
-#   end
+# PHASE 4
+class BestFriend
+  def initialize(name, yrs_known, fav_pastime)
+    @name = name
+    @yrs_known = yrs_known
+    @fav_pastime = fav_pastime
+  end
 
-#   def talk_about_friendship
-#     puts "Wowza, we've been friends for #{@yrs_known}. Let's be friends for another #{1000 * @yrs_known}."
-#   end
+  def talk_about_friendship
+    puts "Wowza, we've been friends for #{@yrs_known}. Let's be friends for another #{1000 * @yrs_known}."
+  end
 
-#   def do_friendstuff
-#     puts "Hey bestie, let's go #{@fav_pastime}. Wait, why don't you choose. 😄"
-#   end
+  def do_friendstuff
+    puts "Hey bestie, let's go #{@fav_pastime}. Wait, why don't you choose. 😄"
+  end
 
-#   def give_friendship_bracelet
-#     puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me." 
-#   end
-# end
+  def give_friendship_bracelet
+    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me." 
+  end
+end
 
 
